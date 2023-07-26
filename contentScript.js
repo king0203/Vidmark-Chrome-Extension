0c2d18573dbd059958e49109fb5509715b0948b5
king0203 <darshanarkhade@gmail.com>
2023-07-27 02:01:11 +0530

    script done

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,10 @@
 (() => {
+    // Declare variables to store DOM elements and video information
     let youtubeLeftControls, youtubePlayer;
     let currentVideo = "";
     let currentVideoBookmarks = [];
   
+    // Function to fetch bookmarks from Chrome storage
     const fetchBookmarks = () => {
       return new Promise((resolve) => {
         chrome.storage.sync.get([currentVideo], (obj) => {
@@ -11,6 +13,7 @@
       });
     };
   
+    // Function to handle adding a new bookmark
     const addNewBookmarkEventHandler = async () => {
       const currentTime = youtubePlayer.currentTime;
       const newBookmark = {
@@ -18,18 +21,43 @@
         desc: "Bookmark at " + getTime(currentTime),
       };
   
+      // Fetch existing bookmarks from Chrome storage
       currentVideoBookmarks = await fetchBookmarks();
   
+      // Add the new bookmark to the list and sort by time
       chrome.storage.sync.set({
         [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
       });
     };
   
+    // Listener for messages from the background script
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+      const { type, value, videoId } = obj;
+  
+      // Handle different message types
+      if (type === "NEW") {
+        currentVideo = videoId;
+        newVideoLoaded();
+      } else if (type === "PLAY") {
+        youtubePlayer.currentTime = value;
+      } else if ( type === "DELETE") {
+        // Remove a bookmark based on the specified time
+        currentVideoBookmarks = currentVideoBookmarks.filter((b) => b.time != value);
+        chrome.storage.sync.set({ [currentVideo]: JSON.stringify(currentVideoBookmarks) });
+  
+        // Send back the updated bookmarks list to the background script
+        response(currentVideoBookmarks);
+      }
+    });
+  
+    // Function to handle a new video loaded
     const newVideoLoaded = async () => {
       const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
   
+      // Fetch existing bookmarks from Chrome storage
       currentVideoBookmarks = await fetchBookmarks();
   
+      // Create the bookmark button and add it to the DOM if it doesn't exist
       if (!bookmarkBtnExists) {
         const bookmarkBtn = document.createElement("img");
   
@@ -45,28 +73,15 @@
       }
     };
   
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-      const { type, value, videoId } = obj;
-  
-      if (type === "NEW") {
-        currentVideo = videoId;
-        newVideoLoaded();
-      } else if (type === "PLAY") {
-        youtubePlayer.currentTime = value;
-      } else if ( type === "DELETE") {
-        currentVideoBookmarks = currentVideoBookmarks.filter((b) => b.time != value);
-        chrome.storage.sync.set({ [currentVideo]: JSON.stringify(currentVideoBookmarks) });
-  
-        response(currentVideoBookmarks);
-      }
-    });
-  
+    // Call newVideoLoaded to handle the initial video load
     newVideoLoaded();
   })();
   
+  // Function to format a time value in "HH:mm:ss" format
   const getTime = t => {
     var date = new Date(0);
     date.setSeconds(t);
   
     return date.toISOString().substr(11, 8);
   };
+  
